f41657f496b3b704c75359f02f51a7db7a613dd2
kshitij-badkas <kshitij_badkas@keemail.me>
2022-11-03 12:06:08 +0530

    Added directory 'Project'. Added ComboBox code.

---
+++ WinDev/Project/ComboBox/MyWindow.rc
@@ -0,0 +1,1 @@
+#include "MyWindow.h"
---
+++ WinDev/Project/ComboBox/Source.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <Windows.h>
+//#define MYICON 101
+BOOL CALLBACK MyDlgProc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
+#define DATAENTRY 10
+
