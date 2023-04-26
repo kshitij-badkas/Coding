d8de65db5d8ed3f70ae6537e9277199f807e3e72
Kshitij Badkas <kshitij.badkas@zohomail.eu>
2023-04-26 10:42:28 +0530

    Removed Project Directory.

--- WinDev/Project/ComboBox/MyWindow.rc
+++
@@ -1,1 +0,0 @@
-#include "MyWindow.h"
--- WinDev/Project/ComboBox/Source.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Windows.h>
-//#define MYICON 101
-BOOL CALLBACK MyDlgProc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
-#define DATAENTRY 10
-
